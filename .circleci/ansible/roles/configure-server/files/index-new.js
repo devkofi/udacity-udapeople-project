ae322c32a224215cfea643889ad71f2f3d121052
Kofi Nyarko Kumi <devkofinyarkokumi@gmail.com>
2022-08-23 09:33:03 +0000

    Fix Deployment Error

---
+++ .circleci/ansible/roles/configure-server/files/index-new.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors({
+    origin: "http://s3-bucket/index.html"
+}));
+
+app.get("/data", (req, res) => {
+    res.json("Project 3 - Udapeople by Kofi Nyarko Kumi!")
+});
+
+app.listen(3030);
--- .circleci/ansible/roles/configure-server/files/index.js
+++ .circleci/ansible/roles/configure-server/files/index.js
@@ -3,4 +3,5 @@ var server = http.createServer(function (req, res) {
 res.writeHead(200);
 res.end("Project 3 - Udapeople by Kofi Nyarko Kumi!");
 });
+
 server.listen(3030);
